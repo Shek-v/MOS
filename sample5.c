9350d2fb09bb53781f6d7f979cf011b6ff0cea5f
Alter <75191632+Shek-v@users.noreply.github.com>
2024-05-13 16:55:55 +0900

    Create sample5.c

---
+++ sample5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+void main()
+{
+    int	a;
+
+    printf("Input an integer number:");
+    scanf("%d",&a);
+
+    if (a > 0) {
+	printf("Number input is Positive!\n");
+    } else {
+	printf("Number input is Negative!\n");
+    }
+}
