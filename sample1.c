732e1c417456e3c37df3ac8047f587bc8d7cfd28
Alter <75191632+Shek-v@users.noreply.github.com>
2024-05-13 16:53:07 +0900

    Create sample1.c

---
+++ sample1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ *   Sample program for arithmetic operators
+ */
+ 
+void main()
+{
+    // Calculation
+    printf("%d + %d = %d\n", 5, 2, 5+2);		/* Add */
+    printf("%d - %d = %d\n", 5, 2, 5-2);		/* Subtract */
+    printf("%d * %d = %d\n", 5, 2, 5*2);		/* Multiply */
+    printf("%d / %d = %d Modulo %d\n", 5, 2, 5/2, 5%2);	/* Divide */  
+}
